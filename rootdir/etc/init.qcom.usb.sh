25e5e2c970f37342f3103de7f810bfcc1c2bb4cb
Jack Pham <jackp@codeaurora.org>
2018-04-10 16:09:36 -0700

    init.qcom.usb: remove target-specific initialization
    Target specific intialization, such as enabling ConfigFS and
    setting the controller name should be done in a per-target's
    init.target.rc on boot section. Remove the setprop calls for
    sys.usb.controller and such from this.
    
    This will also allow enabling PRODUCT_COMPATIBLE_PROPERTY
    which tightens SELinux policy to never allow setting of
    various system/core properties from qti_init_shell context.
    
    Change-Id: I518fc27e7f6057df06847922e14801b4596ee1ca

--- rootdir/etc/init.qcom.usb.sh
+++ rootdir/etc/init.qcom.usb.sh
@@ -138,33 +138,6 @@ case "$usb_config" in
   * ) ;; #USB persist config exists, do nothing
 esac
 
-# set USB controller's device node
-case "$target" in
-    "msm8996")
-        setprop sys.usb.controller "6a00000.dwc3"
-        setprop vendor.usb.rndis.func.name "rndis_bam"
-	setprop vendor.usb.rmnet.func.name "rmnet_bam"
-	;;
-    "msm8998" | "apq8098_latv")
-        setprop sys.usb.controller "a800000.dwc3"
-        setprop vendor.usb.rndis.func.name "gsi"
-	setprop vendor.usb.rmnet.func.name "gsi"
-	;;
-    "sdm660")
-        setprop sys.usb.controller "a800000.dwc3"
-        setprop vendor.usb.rndis.func.name "rndis_bam"
-	setprop vendor.usb.rmnet.func.name "rmnet_bam"
-	echo 15916 > /sys/module/usb_f_qcrndis/parameters/rndis_dl_max_xfer_size
-        ;;
-    "sdm845" | "sdm670")
-        setprop sys.usb.controller "a600000.dwc3"
-        setprop vendor.usb.rndis.func.name "gsi"
-        setprop vendor.usb.rmnet.func.name "gsi"
-        ;;
-    *)
-	;;
-esac
-
 # check configfs is mounted or not
 if [ -d /config/usb_gadget ]; then
 	# Chip-serial is used for unique MSM identification in Product string
@@ -180,17 +153,6 @@ if [ -d /config/usb_gadget ]; then
 		serialno=1234567
 		echo $serialno > /config/usb_gadget/g1/strings/0x409/serialnumber
 	fi
-
-	persist_comp=`getprop persist.sys.usb.config`
-	comp=`getprop sys.usb.config`
-	echo $persist_comp
-	echo $comp
-	if [ "$comp" != "$persist_comp" ]; then
-		echo "setting sys.usb.config"
-		setprop sys.usb.config $persist_comp
-	fi
-
-	setprop sys.usb.configfs 1
 fi
 
 #

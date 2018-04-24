66417c61a64d64d5d8a4d498f98f6c55f6047244
Rajashekar Adi <radi@codeaurora.org>
2018-04-24 15:02:57 +0530

    power: Reference AOSP headers through header lib
    VNDK restrictions doesn't includes standard headers by
    default. Add them explicitly.
    
    Change-Id: I033c3d9c8a1ee1e7f52a6398ae989becd66d161e

--- power/Android.mk
+++ power/Android.mk
@@ -8,6 +8,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
+LOCAL_HEADER_LIBRARIES += libcutils_headers libutils_headers libhardware_headers
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
 LOCAL_C_INCLUDES := external/libxml2/include \
                     external/icu/icu4c/source/common

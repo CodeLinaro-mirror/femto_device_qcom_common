sm6150-dev/android_device_xiaomi_sm6150-common

init.qcom.rc: Add vendor prefix to vendor qti binary
Added vendor prefix to qti module

Change-Id: I7cc642ec8d2ff1610aaf5568f72358336e00adb8

rootdir/etc/init.class_main.sh
@@ -90,11 +90,11 @@ case "$baseband" in
 
     case "$datamode" in
         "tethered")
-            start qti
+            start vendor.qti
             start port-bridge
             ;;
         "concurrent")
-            start qti
+            start vendor.qti
             start netmgrd
             start port-bridge
             ;;

CodeLinaro-mirror/femto_device_qcom_common

rootdir/etc/init.class_main.sh
@@ -90,11 +90,11 @@ case "$baseband" in
 
     case "$datamode" in
         "tethered")
-            start qti
+            start vendor.dataqti
             start vendor.port-bridge
             ;;
         "concurrent")
-            start qti
+            start vendor.dataqti
             start vendor.netmgrd
             start vendor.port-bridge
             ;;
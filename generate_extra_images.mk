d3cefd6c5c7deafb9be3c122076aa277bac7a55c
Smita Ghosh <smitag@codeaurora.org>
2018-06-14 17:41:18 -0700

    Move mount points to vendor.
    /firmware to /vendor/firmware_mnt
    /dsp to /vendor/dsp
    /persist to /mnt/vendor/pesist
    /bt_firmware to /vendor/bt_firmware
    
    Update references of firmware to /vendor/firmware_mnt
    - init.qcom.early_boot.sh
    - init.qcom.sh
    
    Change-Id: I9a038ef21da79a12b7d6f7f64da73f3aa6e4dcd4

--- generate_extra_images.mk
+++ generate_extra_images.mk
@@ -23,9 +23,9 @@ endif
 
 #A/B builds require us to create the mount points at compile time.
 #Just creating it for all cases since it does not hurt.
-FIRMWARE_MOUNT_POINT := $(TARGET_ROOT_OUT)/firmware
-BT_FIRMWARE_MOUNT_POINT := $(TARGET_ROOT_OUT)/bt_firmware
-DSP_MOUNT_POINT := $(TARGET_ROOT_OUT)/dsp
+FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/firmware_mnt
+BT_FIRMWARE_MOUNT_POINT := $(TARGET_OUT_VENDOR)/bt_firmware
+DSP_MOUNT_POINT := $(TARGET_OUT_VENDOR)/dsp
 PERSIST_MOUNT_POINT := $(TARGET_ROOT_OUT)/persist
 ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) \
 				 $(BT_FIRMWARE_MOUNT_POINT) \
@@ -33,23 +33,18 @@ ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MOUNT_POINT) \
 				 $(PERSIST_MOUNT_POINT)
 $(FIRMWARE_MOUNT_POINT):
 	@echo "Creating $(FIRMWARE_MOUNT_POINT)"
-	@mkdir -p $(TARGET_ROOT_OUT)/firmware
-	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/firmware
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware_mnt
 
 $(BT_FIRMWARE_MOUNT_POINT):
 	@echo "Creating $(BT_FIRMWARE_MOUNT_POINT)"
-	@mkdir -p $(TARGET_ROOT_OUT)/bt_firmware
-	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/bt_firmware
+	@mkdir -p $(TARGET_OUT_VENDOR)/bt_firmware
 
 $(DSP_MOUNT_POINT):
 	@echo "Creating $(DSP_MOUNT_POINT)"
-	@mkdir -p $(TARGET_ROOT_OUT)/dsp
-	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/dsp
+	@mkdir -p $(TARGET_OUT_VENDOR)/dsp
 
 $(PERSIST_MOUNT_POINT):
 	@echo "Creating $(PERSIST_MOUNT_POINT)"
-	@mkdir -p $(TARGET_ROOT_OUT)/persist
-	@mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/persist
 
 #----------------------------------------------------------------------
 # Generate secure boot image

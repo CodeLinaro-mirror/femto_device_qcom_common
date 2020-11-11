0791500e74734626a8c712d9f98d99f67d73f937
Yanfei Zhou <zyanfei@codeaurora.org>
2020-11-11 15:47:08 +0800

    Bluetooth: Set BLE_VND_INCLUDED to FALSE
    Change-Id: I3dd9357147a790cbe5a9b1513153427178be2884

--- automotive_tb/bdroid_buildcfg.h
+++ automotive_tb/bdroid_buildcfg.h
@@ -24,7 +24,7 @@
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
-#define BLE_VND_INCLUDED   TRUE
+#define BLE_VND_INCLUDED   FALSE
 // skips conn update at conn completion
 #define BT_CLEAN_TURN_ON_DISABLED 1
 

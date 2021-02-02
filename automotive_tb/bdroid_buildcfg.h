87e48d59bdb480a9355cf830b5b28fa4441d8dd8
Fei Zheng <zfei@codeaurora.org>
2021-02-02 18:53:31 +0800

    common: Bluetooth: automotive_tb: Disable A2DP(SNK) /AVRCP(CT) and change CoD
    1. Disable A2DP(SNK)/AVRCP(CT) for Tablet UI
    
    That is, enable A2DP(SRC)/AVRCT(TG)
    
    2. Set CoD for Tablet UI
    
    Change-Id: I88f1a387c56d7b5fe1414a6d53e56c293e4f67a0

--- automotive_tb/bdroid_buildcfg.h
+++ automotive_tb/bdroid_buildcfg.h
@@ -32,7 +32,7 @@
 #define AVDT_NUM_SEPS 12
 
 /* Enable A2DP(SNK) */
-#define BTA_AV_SINK_INCLUDED TRUE
+#define BTA_AV_SINK_INCLUDED FALSE
 
 /* Enable delay-report for A2DP(SNK) */
 #define A2DP_SINK_DELAY_REPORT TRUE
@@ -41,16 +41,17 @@
 #define BTA_EIR_SUPPORT_128BIT_UUID TRUE
 
 /*
-* Change CoD for Auto car-kit
+* Default class of device
 * {SERVICE_CLASS, MAJOR_CLASS, MINOR_CLASS}
 *
-* SERVICE_CLASS:0x26 (Bit17 - Networking, Bit18 - Rendering, Bit21 - Audio)
-* MAJOR_CLASS:0x04 - Audio/Video
-* MINOR_CLASS:0X08 - Hands-free Device
+* SERVICE_CLASS:0x5A (Bit17 -Networking,Bit19 - Capturing,Bit20 -Object
+* Transfer,Bit22 -Telephony)
+* MAJOR_CLASS:0x01 - Computer
+* MINOR_CLASS:0x03 - Tablet
 *
 * Detailed information can be found in below link
 * https://www.bluetooth.com/specifications/assigned-numbers/baseband
 */
 #define BTA_DM_COD \
-  { 0x26, 0x04, 0x08 }
+  { 0x5A, 0x01, 0x03 }
 #endif

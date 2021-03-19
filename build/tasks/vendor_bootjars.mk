55a27455b377956c15e28f5f389e87dd856ae0e8
Vijayakumar Badiger <vbadig@codeaurora.org>
2021-03-18 22:30:21 -0700

    fix compilation issues
    Change-Id: I5bf1c4d2747c4b7740aec6e209a15c77bd34e049

--- build/tasks/vendor_bootjars.mk
+++ build/tasks/vendor_bootjars.mk
@@ -5,7 +5,7 @@ ifneq ($(VENDOR_QTI_PLATFORM),qssi)
 ifneq ($(call is-vendor-board-platform,QCOM),true)
 
 #call dex_preopt.mk for extra jars
-include $(BUILD_SYSTEM)/dex_preopt.mk
+#include $(BUILD_SYSTEM)/dex_preopt.mk
 
 endif
 endif

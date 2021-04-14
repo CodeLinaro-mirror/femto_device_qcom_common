4b463eec1d30596b133a49cef0a439d90d07e216
Rajeswari Konda <rajkonda@codeaurora.org>
2021-04-14 18:05:27 +0530

    common: added init.testscripts.sh
    Change-Id: I911a2a9a0c40375fb622774e8de614184ca6eead

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -140,6 +140,14 @@ LOCAL_SRC_FILES    := etc/init.qcom.debug.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.testscripts.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.testscripts.sh
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.debug-sdm660.sh
 LOCAL_MODULE_TAGS  := optional

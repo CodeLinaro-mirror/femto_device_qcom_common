76cee4f6fcd6809449307dcafab47dc74e011fa8
Abhiprayah Tiwari <abhipray@codeaurora.org>
2021-08-18 16:17:09 +0530

    common: Add keyboard.mk back for auto targets
    This file got removed due to aosp_base_telephony.mk removal.
    This file allows scrcpy to take inputs from keyboard and allows
    input text command to work.
    
    Change-Id: I91e959039f578f0842f69016d912c7502bdc6aff

--- common64.mk
+++ common64.mk
@@ -13,6 +13,8 @@ ifeq ($(TARGET_BOARD_AUTO),true)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, frameworks/base/data/fonts/fonts.mk)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
+$(call inherit-product-if-exists, external/hyphenation-patterns/patterns.mk)
+$(call inherit-product-if-exists, frameworks/base/data/keyboards/keyboards.mk)
 
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \

7721e8a053add0092b1de701f30dd4461977f753
Abhiprayah Tiwari <abhipray@codeaurora.org>
2021-08-26 11:08:08 +0530

    common: Add fonts back
    If fonts are missing then some apps may fail to show text
    properly and only blank boxes will be shown instead.
    
    Change-Id: I9e30e2082491830ca55846398b8ce7cc28bad19d

--- common64.mk
+++ common64.mk
@@ -15,6 +15,7 @@ $(call inherit-product, frameworks/base/data/fonts/fonts.mk)
 $(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
 $(call inherit-product-if-exists, external/hyphenation-patterns/patterns.mk)
 $(call inherit-product-if-exists, frameworks/base/data/keyboards/keyboards.mk)
+$(call inherit-product-if-exists, external/noto-fonts/fonts.mk)
 
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \

62be8bba2068b850d015f5f76059953381c56de3
Naga Bhavani Akella <quic_nakella@quicinc.com>
2022-01-20 10:52:26 +0530

    Bluetooth Offload Service: Permission and manifest changes
    Change-Id: I7a7784d1c88e8998cd35d442947fed97ecea438c

--- config.fs
+++ config.fs
@@ -52,6 +52,18 @@ user: AID_BLUETOOTH
 group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
+[vendor/bin/hw/vendor.qti.hardware.bluetooth_offload@1.0-service-qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND
+
+[vendor/bin/hw/vendor.qti.hardware.bluetooth_offload@1.0-service-qti-lazy]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND
+
 [system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
 mode: 0755
 user: AID_SYSTEM

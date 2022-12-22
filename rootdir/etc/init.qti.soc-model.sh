20c00a57fea994de9984a5dfb5a62ec468f85746
Ashok Mutyala <quic_amutyala@quicinc.com>
2022-12-22 11:50:40 +0530

    device-common: Add ro.soc.model based on chipsets
    Change-Id: I7e1769888116db00b5c602658ee94469f422bb2c
    Signed-off-by: Ashok Mutyala <quic_amutyala@quicinc.com>

---
+++ rootdir/etc/init.qti.soc-model.sh
@@ -0,0 +1,22 @@
+#! /vendor/bin/sh
+
+# Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+# Add New Chipsets bases on soc_id when testChipsetinfo Fails
+
+if [ -f /sys/devices/soc0/soc_id ]; then
+    soc_id=`cat /sys/devices/soc0/soc_id`
+else
+    soc_id=`cat /sys/devices/system/soc/soc0/id`
+fi
+
+if [ "$soc_id" == "340" ] | [ "$soc_id" == "405" ]; then
+    setprop ro.vendor.qti.soc_model SA8195P
+elif [ "$soc_id" == "460" ]; then
+    setprop ro.vendor.qti.soc_model SA8295P
+elif [ "$soc_id" == "367" ] | [ "$soc_id" == "362" ]; then
+    setprop ro.vendor.qti.soc_model SA8155P
+fi
+
+

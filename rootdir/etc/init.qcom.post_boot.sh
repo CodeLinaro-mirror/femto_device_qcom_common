a559655d98c5b254895ec6eba4ad213da11b9e56
Richard J K <quic_rjk@quicinc.com>
2023-08-05 02:09:04 +0530

    Revert "Invalid target_variant check in init.qcom.post_boot.sh."
    This reverts commit 564020ad399f5e2dc21750215b7433a59f6bf1ec.
    
    Change-Id: Iea12b84f2312f6b9fb3d045a8f21eaf436da5ee5

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -5113,8 +5113,7 @@ esac
 case "$target" in
     "msmnile")
 	# cpuset parameters
-	target_suffix=`getprop ro.vendor.board.suffix`
-	target_varient=$target$target_suffix
+	target_varient=`getprop ro.build.product`
         if [ "$target_varient" == "msmnile_gvmq" ]; then
 		echo 4-7 > /dev/cpuset/background/cpus
 		echo 4-7 > /dev/cpuset/system-background/cpus

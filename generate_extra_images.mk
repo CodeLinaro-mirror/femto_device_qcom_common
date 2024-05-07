samsung-sm8550-dev/device_qcom_common

common: Update metadata to use either ext4 or f2fs
Currently the filesystem, the metadata partition is generated in is
ext4. The following change is to provide options to generate
metadata either as an ext4 or f2fs file system.

Change-Id: Id6ef07de9202556544ea6eb2425c6287ec14d0f6

generate_extra_images.mk
@@ -75,21 +75,38 @@ endif
 # As of now this in empty at build and data is runtime generated only,
 # so create an empty fs
 #----------------------------------------------------------------------
+ifneq ($(BOARD_USES_METADATA_PARTITION),)
 ifneq ($(strip $(BOARD_METADATAIMAGE_PARTITION_SIZE)),)
 
 TARGET_OUT_METADATA := $(PRODUCT_OUT)/metadata
 
 INSTALLED_METADATAIMAGE_TARGET := $(PRODUCT_OUT)/metadata.img
 
+ifeq ($(BOARD_METADATAIMAGE_FILE_SYSTEM_TYPE),ext4)
 define build-metadataimage-target
     $(call pretty,"Target metadata fs image: $(INSTALLED_METADATAIMAGE_TARGET)")
     @mkdir -p $(TARGET_OUT_METADATA)
-    $(hide)PATH=$(HOST_OUT_EXECUTABLES):$${PATH} $(MKEXTUSERIMG) -s $(TARGET_OUT_METADATA) $@ ext4 metadata $(BOARD_METADATAIMAGE_PARTITION_SIZE)
+    $(hide)PATH=$(HOST_OUT_EXECUTABLES):$${PATH} $(MKEXTUSERIMG) -s $(TARGET_OUT_METADATA) $@ $(BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE) metadata $(BOARD_METADATAIMAGE_PARTITION_SIZE)
     $(hide) chmod a+r $@
 endef
 
 $(INSTALLED_METADATAIMAGE_TARGET): $(MKEXTUSERIMG) $(MAKE_EXT4FS)
 	$(build-metadataimage-target)
+
+else
+ifeq ($(BOARD_METADATAIMAGE_FILE_SYSTEM_TYPE),f2fs)
+define build-metadataimage-target
+    $(call pretty,"Target metadata fs image: $(INSTALLED_METADATAIMAGE_TARGET)")
+    @mkdir -p $(TARGET_OUT_METADATA)
+    $(hide)PATH=$(HOST_OUT_EXECUTABLES):$${PATH} $(MKF2FSUSERIMG) $(INSTALLED_METADATAIMAGE_TARGET) $(BOARD_METADATAIMAGE_PARTITION_SIZE) -S -f $(TARGET_OUT_METADATA) -t metadata -L metadata $@
+    $(hide) chmod a+r $@
+endef
+
+$(INSTALLED_METADATAIMAGE_TARGET): $(MKF2FSUSERIMG)
+	$(build-metadataimage-target)
+
+endif
+endif
 
 ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_METADATAIMAGE_TARGET)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_METADATAIMAGE_TARGET)
@@ -99,6 +116,7 @@ droidcore-unbundled: $(INSTALLED_METADATAIMAGE_TARGET)
 .PHONY: metadataimage
 metadataimage: $(INSTALLED_METADATAIMAGE_TARGET)
 
+endif
 endif
 
 #----------------------------------------------------------------------

CodeLinaro-mirror/femto_device_qcom_common

(cherry picked from commit 002ddfd11d8611d578352a69194b280030c1fa00)
Signed-off-by: Roshan Dsouza <quic_roshdsou@quicinc.com>
(cherry picked from commit 38e9bc2feb0a47214c25ba4d60d1488f87262714)

generate_extra_images.mk
@@ -74,12 +74,14 @@ endif
 # As of now this in empty at build and data is runtime generated only,
 # so create an empty fs
 #----------------------------------------------------------------------
+ifneq ($(BOARD_USES_METADATA_PARTITION),)
 ifneq ($(strip $(BOARD_METADATAIMAGE_PARTITION_SIZE)),)
 
 TARGET_OUT_METADATA := $(PRODUCT_OUT)/metadata
 
 INSTALLED_METADATAIMAGE_TARGET := $(PRODUCT_OUT)/metadata.img
 
+ifneq ($(BOARD_METADATAIMAGE_FILE_SYSTEM_TYPE),f2fs)
 define build-metadataimage-target
     $(call pretty,"Target metadata fs image: $(INSTALLED_METADATAIMAGE_TARGET)")
     @mkdir -p $(TARGET_OUT_METADATA)
@@ -90,6 +92,19 @@ endef
 $(INSTALLED_METADATAIMAGE_TARGET): $(MKEXTUSERIMG) $(MAKE_EXT4FS)
 	$(build-metadataimage-target)
 
+else
+define build-metadataimage-target
+    $(call pretty,"Target metadata fs image: $(INSTALLED_METADATAIMAGE_TARGET)")
+    @mkdir -p $(TARGET_OUT_METADATA)
+    $(hide)PATH=$(HOST_OUT_EXECUTABLES):$${PATH} $(MKF2FSUSERIMG) $(INSTALLED_METADATAIMAGE_TARGET) $(BOARD_METADATAIMAGE_PARTITION_SIZE) -S -f $(TARGET_OUT_METADATA) -t metadata -L metadata $@
+    $(hide) chmod a+r $@
+endef
+
+$(INSTALLED_METADATAIMAGE_TARGET): $(MKF2FSUSERIMG)
+	$(build-metadataimage-target)
+
+endif
+
 ALL_DEFAULT_INSTALLED_MODULES += $(INSTALLED_METADATAIMAGE_TARGET)
 ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_METADATAIMAGE_TARGET)
 droidcore: $(INSTALLED_METADATAIMAGE_TARGET)
@@ -97,6 +112,7 @@ droidcore: $(INSTALLED_METADATAIMAGE_TARGET)
 .PHONY: metadataimage
 metadataimage: $(INSTALLED_METADATAIMAGE_TARGET)
 
+endif
 endif
 
 #----------------------------------------------------------------------
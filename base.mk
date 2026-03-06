93788edc24c2ac4776bb9c9d273edb23586e95ab
Manjunatha Madana <c_mamanj@qti.qualcomm.com>
2026-03-06 18:12:07 +0530

    common: Android 17 bringup change.
    Remove duplicate property assignment for vendor.usb.diag.func.name
    
    Change-Id: I7524dea6a5a55230257c2d6e5e6da6d384267df9

--- base.mk
+++ base.mk
@@ -1109,7 +1109,7 @@ ifeq ($(TARGET_USES_QCOM_BSP_ATEL),true)
     PRODUCT_PROPERTY_OVERRIDES += persist.radio.multisim.config=dsds
 endif
 
-ifeq ( ,$(filter 12 S 13 T U 14 UpsideDownCake V VanillaIceCream 15 W Baklava 16,$(PLATFORM_VERSION)))
+ifeq ( ,$(filter 12 S 13 T U 14 UpsideDownCake V VanillaIceCream 15 W Baklava 16 CinnamonBun 17,$(PLATFORM_VERSION)))
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     vendor.usb.diag.func.name=diag
 endif
